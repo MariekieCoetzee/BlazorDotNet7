eb1c1c12937d7b2d5df2a90b8d8418e3891f1164
Mariekie Coetzee <mariekie@mariekiecoetzee.com>
2022-11-11 10:06:45 +1000

    initial commit

---
+++ BlazorDotNet7/Models/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace BlazorDotNet7.Models
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+        public string? Detail { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # BlazorDotNet7
-Tech spike for new features in .net 7 for Blazor
